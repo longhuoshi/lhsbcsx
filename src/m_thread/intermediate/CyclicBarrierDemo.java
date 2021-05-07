1689624734d32d43a6323526ea990535c8915507
lhs <1191130063@qq.com>
2021-05-07 14:32:27 +0800

    update 同步计数器，完

---
+++ src/m_thread/intermediate/CyclicBarrierDemo.java
@@ -0,0 +1,62 @@
+package m_thread.intermediate;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @author l
+ * @date 2021/5/7 11:43
+ * @description
+ *
+ * 5.12 同步计数器CyclicBarrier
+ *
+ * 使用场景：大数据运算需要拆分成多步骤的时候，比如这么一个实际应用场景：我们需要统计全国的
+ * 业务数据，其中各省的数据库是独立的，也就是说按省分库，并且统计的数据量很大，统计过程也比
+ * 较慢。为了提高性能，快速计算，我们采取并发的方式，多个线程同时计算各省数据，每个省下面又
+ * 用多线程，最后再汇总统计
+ *
+ */
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+        CyclicBarrier barrier = new CyclicBarrier(4,new TotalTask());
+        BillTask worker = new BillTask("111",barrier);
+        BillTask worker2 = new BillTask("222",barrier);
+        BillTask worker3 = new BillTask("333",barrier);
+        worker.start();
+        worker2.start();
+        worker3.start();
+        System.out.println("Main thread end! ");
+
+
+    }
+
+    static class TotalTask extends Thread{
+        public void run(){
+            System.out.println("所有子任务都执行完了，就开始执行主任务了");
+        }
+    }
+
+    static class BillTask extends Thread{
+        private String billName;
+        private CyclicBarrier barrier;
+        public BillTask(String workerName,CyclicBarrier barrier){
+            billName = workerName;
+            this.barrier = barrier;
+        }
+
+        public void run(){
+            try {
+                System.out.println("市区："+billName+"运算开始：");
+                Thread.sleep(1000L);//模仿第一次运算
+                System.out.println("市区："+billName+"运算完成，等待...");
+                //假设一次运算不完，第二次要依赖第一次的运算结果 ，
+                //都到达这个节点之后后面才会继续执行。
+                barrier.await();
+                System.out.println("全部都结束 ，市区"+billName+"才开始后面的工作。");
+
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/m_thread/intermediate/SemaphoreDemo.java
@@ -0,0 +1,43 @@
+package m_thread.intermediate;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author l
+ * @date 2021/5/7 11:09
+ * @description
+ *
+ * 5.11 同步计数器Semaphore
+ *
+ * 使用场景：排队场景，资源有限的房间，资源有限的群等等。
+ * 常见的实际应用场景包括线程池，连接池等。
+ *
+ * 实例：假设一个服务器资源有限，只允许同时3个人进行访问，一共来了10个人的场景 。
+ *
+ */
+public class SemaphoreDemo {
+    public static void main(String[] args) {
+        final Semaphore semaphore = new Semaphore(3);//一次只运行3个人进行访问。
+        for (int i = 0; i < 10; i++) {
+            final int no = i;
+            Runnable thread = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("用户"+no+"连接上了：");
+                        Thread.sleep(300L);
+                        semaphore.acquire();//获取接下来执行的许可
+                        System.out.println("用户"+no+"开始访问后台程序...");
+                        Thread.sleep(1000L);//模仿用户访问服务器过程
+                        semaphore.release();//释放，允许下一个线程访问进入后台
+                        System.out.println("用户"+no+"访问结束 。");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            new Thread(thread).start();
+
+        }
+    }
+}
