848711fb9050f24597c706fb7f7b8b24e77a6a27
Mars <lhuoshi@163.com>
2021-09-24 08:10:33 +0800

    update jvm

---
+++ src/main/java/jvm_demo/chapter16/OperandStackTest.java
@@ -0,0 +1,30 @@
+package jvm_demo.chapter16;
+
+/**
+ *53-涉及操作数栈的字节码指令执行分析
+ * */
+public class OperandStackTest {
+    public void testAddOperation(){
+        //byte,short,char,boolean:都是以int型来保存的
+        byte i = 15;
+        int j = 8;
+        int k = i +j ;
+
+        //int m = 8;  //以bipush  压栈
+        //int m = 800;  //以sipush   压栈
+    }
+
+    public int getSum(){
+        int m = 10;
+        int n = 20;
+        int k = m+n ;
+        return k;
+    }
+    public void testGetSum(){
+        //获取上一个栈帧返回的结果，并保存在操作数栈中。
+        int i = getSum();
+        int j = 10;
+
+    }
+
+}
