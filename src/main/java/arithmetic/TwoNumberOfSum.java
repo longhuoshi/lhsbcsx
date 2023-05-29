f8c7d39f38d6f374f7ab987f9bb86cfa08ae0fcc
lhs <1191130063@qq.com>
2023-05-29 10:44:47 +0800

    两数之和

---
+++ src/main/java/arithmetic/TwoNumberOfSum.java
@@ -0,0 +1,41 @@
+package arithmetic;
+
+import java.util.HashMap;
+
+/**
+ * @author l
+ * @date 2023/5/29 10:30
+ * @description
+ *
+ *
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ *
+ * 你可以按任意顺序返回答案。
+ *
+ */
+public class TwoNumberOfSum {
+    public static void main(String[] args) {
+        TwoNumberOfSum t = new TwoNumberOfSum();
+        int[] nums = {2,5,8,20,3,30};
+        int[] ints = t.twoSum(nums, 5);
+        for (int anInt : ints) {
+            System.out.println(anInt);
+        }
+
+
+    }
+
+    public int[]  twoSum(int[] nums , int target){
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target-nums[i])){
+                return new int[]{hashMap.get(target-nums[i]),i};
+            }
+            hashMap.put(nums[i],i);
+        }
+
+        return new int[]{};
+    }
+}
